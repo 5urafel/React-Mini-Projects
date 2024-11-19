76cb505964f0170a59fc9150f8586246d9ff30ec
5urafel <surafel5540f@gmail.com>
2024-11-19 14:15:07 +0000

    final commit
    Simple Birthday reminder to render list of users and clearing all list of users using useState

--- 01_Birthday_Reminder/src/assets/components/card/Card.jsx
+++ 01_Birthday_Reminder/src/assets/components/card/Card.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import { useState } from "react";
 import "./card.css";
+import People from "../../../data";
 
 const Card = () => {
+  const [people, setPeople] = useState(People);
+
+  const handleClick = () => {
+    setPeople([]);
+  };
+
   return (
     <div className="card">
       <h1 className="title">5 Birthdays Today</h1>
       <div className="usersList">
-        <div className="userContainer">
-          <img src="../../../public/hero.svg" alt="" className="userImg" />
-          <div className="userDesc">
-            <h3 className="userName">Surafel Destaw</h3>
-            <h3 className="userAge">24 yesrs</h3>
+        {people.map((user) => (
+          <div className="userContainer" key={user.id}>
+            <div className="userImg">
+              <img src={user.image} alt="" />
+            </div>
+
+            <div className="userDesc">
+              <h3 className="userName">{user.name}</h3>
+              <p className="userAge">{user.age} years</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-      <button className="clrBtn">Clear All</button>
+      <button className="clrBtn" onClick={handleClick}>
+        Clear All
+      </button>
     </div>
   );
 };
