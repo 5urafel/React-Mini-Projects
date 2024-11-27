6645e71de0feaaf22c744c09eafb1e8e949947bd
5urafel <surafel5540f@gmail.com>
2024-11-27 10:34:05 +0000

    Review Completed

--- 03-Reviews/src/App.jsx
+++ 03-Reviews/src/App.jsx
@@ -3,9 +3,9 @@ import Card from "./assets/component/Card/Card";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Card />
-    </>
+    </div>
   );
 }
 
--- 03-Reviews/src/assets/component/Card/Card.jsx
+++ 03-Reviews/src/assets/component/Card/Card.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Card.css";
+import { FaQuoteRight, FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import reviews from "../../../data";
 
 const Card = () => {
-  return <div>Card</div>;
+  const [index, setIndex] = useState(0);
+
+  const handleDecrease = () => {
+    if (index === 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  const handleIncrease = () => {
+    if (index === reviews.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const handleSurprise = () => {
+    const randomNumber = Math.floor(Math.random() * reviews.length);
+    setIndex(randomNumber);
+  };
+
+  return (
+    <div className="card-wrapper">
+      <div className="imgContainer">
+        <div className="quoteIcon">
+          <FaQuoteRight />
+        </div>
+        <img src={reviews[index].image} alt="" className="profileImg" />
+      </div>
+
+      <h3>{reviews[index].name}</h3>
+      <h4>{reviews[index].job}</h4>
+      <p>{reviews[index].text}</p>
+
+      <div className="swipe-icons">
+        <div className="left-icon chevron-icon" onClick={handleDecrease}>
+          <FaChevronLeft />
+        </div>
+
+        <div
+          className="right-icon chevron-icon"
+          onClick={() => handleIncrease()}
+        >
+          <FaChevronRight />
+        </div>
+      </div>
+      <button onClick={handleSurprise}>surprise me</button>
+    </div>
+  );
 };
 
 export default Card;
