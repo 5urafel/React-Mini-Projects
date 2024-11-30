2338d4aaa817405192e5f02b2e6864106c92bcc0
5urafel <surafel5540f@gmail.com>
2024-11-30 21:16:32 +0000

    02-tours-fetching the data from api end point
    replacing the demmy data in the src folder by an api that is live on the internet, used react fetch to fetch the data

--- 02-tours/src/assets/components/List/List.jsx
+++ 02-tours/src/assets/components/List/List.jsx
@@ -2,14 +2,42 @@ import React, { useState } from "react";
 import "./List.css";
 import Card from "../Card/Card";
 import tourData from "../../../data";
+import { useEffect } from "react";
+const url = "https://course-api.com/react-tours-project";
 
 const List = () => {
-  const [places, setPlaces] = useState(tourData);
+  const [loading, setLoading] = useState(true);
+  const [places, setPlaces] = useState([]);
 
   const handleRemove = (id) => {
-    const updatedPlaces = places.filter((place) => place.id != id);
+    const updatedPlaces = places.filter((place) => place.id !== id);
     setPlaces(updatedPlaces);
   };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPlaces(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="listContainer">
